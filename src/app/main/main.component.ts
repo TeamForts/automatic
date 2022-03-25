e52fa04f294493f9aa3191605e37fe5a761d1485
Denis <dantesss0405@gmail.com>
2022-03-25 18:03:07 +0300

    feat(app): Построил граф

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,9 +3,8 @@ import {AppService} from '../entities/services/app.service';
 import {Router} from '@angular/router';
 import * as d3 from 'd3'
 import * as scale from 'd3-scale'
-import * as force from 'd3-force';
 import {Platform} from "@ionic/angular";
-import {GRAPH} from "../../mocks/graph.const";
+import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-main',
@@ -17,29 +16,63 @@ export class MainComponent implements OnInit {
   public graph: any;
   public width: any;
   public height: any;
+  public fg: FormGroup;
+
+  public xMin = Infinity;
+  public xMax = -Infinity;
+  public yMin = Infinity;
+  public yMax = -Infinity;
+
+  public xStart = null;
+  public yStart = null;
+  public graphIsBuild = false;
+
   private _svg: any;
 
 
   constructor(
     private _appService: AppService,
     private _route: Router,
-    private _platform: Platform) {
+    private _platform: Platform,
+    private _fb: FormBuilder
+  ) {
+    this.createForm();
     _platform.ready().then((readySource) => {
      let x = 6108/_platform.width();
      this.width = _platform.width();
      this.height = 5650 / x;
-     this._createSvg();
+     this.createSvg();
     });
   }
 
   ngOnInit() {
     this._appService.startCoordinate$.subscribe( res => {
-   //   res ? console.log(res) : this._route.navigate(['/home']);
+      if ( res ){
+        this.xStart = res.x;
+        this.yStart = res.y;
+        alert(`Ваши координаты x = ${this.xStart} , y = ${this.yStart}`);
+      } else {
+        this._route.navigate(['/home']);
+      }
     });
+
 
   }
 
-  private _createSvg(): void {
+  /**
+   * Создаем форму для input
+   */
+  public createForm(): void {
+    this.fg = this._fb.group({
+      startIndex: [null, Validators.required],
+      endIndex: [null, Validators.required],
+    })
+  }
+
+  /**
+   * Создаем svg
+   */
+  public createSvg(): void {
    this._svg = d3.selectAll('figure#graph')
      .append('svg')
      .attr('width', this.width)
@@ -50,41 +83,53 @@ export class MainComponent implements OnInit {
      .attr('class', 'links');
    d3.select('svg')
      .append('g')
-     .attr('class', 'nodes')
+     .attr('class', 'nodes');
+    d3.select('svg')
+      .append('g')
+      .attr('class', 'minPath')
+      .attr( 'opacity', '1');
 
 
     this.graph = this._appService.getGraphMap();
-    this._createGraph();
+    this.graph.nodes = this.graph.nodes.map( (el, index) => {
+      el.index = index;
+      return el;
+    });
+    this.graphIsBuild = this.createGraph();
   }
 
 
-  private _createGraph(): void{
-    let xMin = 99999999;
-    let xMax = -99999999;
-    let yMin = 99999999;
-    let yMax = -99999999;
+  /**
+   * Создаем граф
+   */
+  public createGraph(): boolean {
+
+    let graph = this.graph;
+
+    // Ищем минимум/максимум в координатах
 
     this.graph.nodes.forEach( (el) => {
-      if ( xMin > el.x ) {
-        xMin = el.x;
+      if ( this.xMin > el.x ) {
+        this.xMin = el.x;
       }
-      if ( xMax < el.x) {
-        xMax = el.x;
+      if ( this.xMax < el.x) {
+        this.xMax = el.x;
       }
-      if ( yMin > el.y ) {
-        yMin = el.y;
+      if ( this.yMin > el.y ) {
+        this.yMin = el.y;
       }
-      if ( yMax < el.y) {
-        yMax = el.y;
+      if ( this.yMax < el.y) {
+        this.yMax = el.y;
       }
     });
-    console.log(xMin);
-    console.log(xMax);
+
+    // Преобразуем координаты чтобы они входили в область svg
+
     let linearX = scale.scaleLinear()
-      .domain([xMax * (-1),xMin * (-1) ])
+      .domain([this.xMax * (-1),this.xMin * (-1) ])
       .range([-4, 104])
     let linearY = scale.scaleLinear()
-      .domain([yMin,yMax ])
+      .domain([this.yMin,this.yMax ])
       .range([0, 100])
 
     for (let i = 0; i < this.graph.links.length; i++){
@@ -98,76 +143,221 @@ export class MainComponent implements OnInit {
       this.graph.nodes[i].x = linearX(  this.graph.nodes[i].x * (-1));
       this.graph.nodes[i].y = linearY( this.graph.nodes[i].y );
     }
-   // console.log('Posle',this.graph.links);
-   // console.log('dd',this.graph.nodes);
-    let graph = this.graph;
 
-/*
-    let simulation = force.forceSimulation()
-      .nodes(graph.nodes)
-      .force('charge', force.forceManyBody().strength(-30))
-      .force('center', force.forceCenter(this.width / 2, this.height / 2))
-      .force('link', force.forceLink()
-        .links(graph.links)
-        .id(function(d: any) {
-        return d.id})
-        .strength( function (d: any, i) {
-          return d.weight;
-        })
-      )
-      .force('x', force.forceX().x(function(d: any, i) {
-        return graph.links[i].center.x;
-      }))
-      .force('y', force.forceY().y(function(d: any, i) {
-        return graph.links[i].center.y;
-      }))
-      .on('tick', ticked);
-*/
+    // Добавляем ребра
 
-   // function updateLinks() {
-      let links = d3.select('.links')
-        .selectAll('line')
-        .data(graph.links)
-        .join('line')
-        .attr('x1', function(d: any) {
-          return d.coords[0].x;
-        })
-        .attr('y1', function(d: any) {
-          return d.coords[0].y
-        })
-        .attr('x2', function(d: any) {
-          return d.coords[1].x
-        })
-        .attr('y2', function(d: any) {
-          return d.coords[1].y
-        })
-        .attr("stroke", "red")
-        .attr("stroke-width", 0.2);
-  //  }
+    let links = d3.select('.links')
+      .selectAll('line')
+      .data(graph.links)
+      .join('line')
+      .attr('x1', function(d: any) {
+        return d.coords[0].x;
+      })
+      .attr('y1', function(d: any) {
+        return d.coords[0].y
+      })
+      .attr('x2', function(d: any) {
+        return d.coords[1].x
+      })
+      .attr('y2', function(d: any) {
+        return d.coords[1].y
+      })
+      .attr("stroke", "red")
+      .attr("stroke-width", 0.2);
 
-   // function updateNodes() {
-     let nodes = d3.select('.nodes')
-        .selectAll('circle')
-        .data(graph.nodes)
-        .enter()
-        .append('circle')
-        .attr('cx', function(d: any) {
-          return d.x
-        })
-        .attr('cy', function(d: any) {
-          return d.y
-        })
-        .attr('r', function(d) {
-          return 0.2
-        })
-       .attr('fill','green');
-   // }
+    // Добавляем вершины
 
-/*    function ticked() {
-      updateLinks()
-      updateNodes()
-    }*/
+    let nodes = d3.select('.nodes')
+      .selectAll('circle')
+      .data(graph.nodes)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d: any) {
+        return d.x
+      })
+      .attr('cy', function(d: any) {
+        return d.y
+      })
+      .attr('r', function(d) {
+        return 0.1
+      })
+      .attr('fill','green');
+
+    return true;
   }
 
 
+  /**
+   * Строим путь от начальной точки до конечной
+   * @param start - индекс начальной точки
+   * @param end - индекс конечной точки
+   */
+  public buildPath(start = 20, end = 150): void {
+
+    start = +this.fg.controls['startIndex'].value;
+    end = +this.fg.controls['endIndex'].value;
+
+    if ( this.graph.nodes.find( el => start === el.index) && this.graph.nodes.find( el => end === el.index) )
+    {
+      d3.select('.minPath')
+        .selectAll('path')
+        .remove();
+      let indexNodes = this.getPath(this.createAdjacencyMatrix(), start, end);
+
+      indexNodes = indexNodes.map(indexNode => {
+        return this.graph.nodes.find(node => {
+          if (node.index === indexNode) {
+            // console.log(node.index);
+            return node;
+          }
+        })
+      }).reverse();
+
+      let line = d3.line()
+        .x(function (d: any) {
+          return d.x;
+        })
+        .y(function (d: any) {
+          return d.y;
+        });
+      let path = d3.select('.minPath')
+        .append('path')
+        .attr("d", line(indexNodes))
+        .attr("stroke", "white")
+        .attr("stroke-width", 1)
+        .attr('fill', 'none');
+    } else {
+      alert('Индекс должен быть числом от 0 до 198');
+    }
+  }
+
+
+  /**
+   * Создание матрицы смежности из графа
+   * @private
+   */
+  public createAdjacencyMatrix(): any {
+    let matrix = [];
+    let size =  this.graph.nodes.length;
+
+    this.graph.links = this.graph.links.map( el => {
+      el.weight = Math.trunc(el.weight);
+      return el;
+    })
+
+    for (let i = 0; i < size; i++) {
+      matrix.push([]);
+      for (let j = 0; j < size; j++) {
+        matrix[i][j] = 0;
+      }
+    }
+
+    this.graph.nodes.forEach( ( node ) => {
+        this.graph.links.forEach( (el) => {
+           if ( node.id === el.source || node.id === el.target) {
+             let targetNode = this.graph.nodes.find( node => node.id === el.target);
+              addEdge(node.index, targetNode.index, el.weight);
+           }
+        })
+    })
+
+    function addEdge(vertex1, vertex2, weight = 0) {
+      if (vertex1 > size - 1 || vertex2 > size - 1) {
+        console.log('invalid vertex');
+      } else if (vertex1 === vertex2) {
+        console.log('same vertex');
+      } else {
+        matrix[vertex1][vertex2] = weight;
+        matrix[vertex2][vertex1] = weight;
+      }
+    }
+
+    return matrix;
+  }
+
+  /**
+   * Алгоритм поиска минимального пути
+   * @param matrix матрица смежности
+   * @param start индекс начальной точки
+   * @param end индекс конечной точки
+   */
+
+  public getPath(matrix, start = 0, end = 1): any {
+    let minDistance = []; // минимальное расстояние
+    let visitedNodes = []; // посещенные вершины
+    let temp, minindex, min;
+    let begin_index = start;
+
+    //Инициализация вершин и расстояний
+    for (let i = 0; i < matrix.length; i++) {
+      minDistance[i] = Infinity;
+      visitedNodes[i] = 1;
+    }
+    minDistance[begin_index] = 0;
+    // Шаг алгоритма
+     do {
+      minindex = Infinity;
+      min = Infinity;
+      for (let i = 0; i < matrix.length; i++){
+        // Если вершину ещё не обошли и вес меньше min
+        if ((visitedNodes[i] == 1) && (minDistance[i]<min)){
+          // Переприсваиваем значения
+          min = minDistance[i];
+          minindex = i;
+        }
+      }
+      // Добавляем найденный минимальный вес
+      // к текущему весу вершины
+      // и сравниваем с текущим минимальным весом вершины
+      if (minindex != Infinity) {
+        for (let i = 0; i < matrix.length; i++)
+        {
+          if (matrix[minindex][i] > 0)
+          {
+            temp = min + matrix[minindex][i];
+            if (temp < minDistance[i])
+            {
+              minDistance[i] = temp;
+            }
+          }
+        }
+        visitedNodes[minindex] = 0;
+      }
+    } while (minindex < Infinity);
+    // Вывод кратчайших расстояний до вершин
+    // console.log("\nКратчайшие расстояния до вершин: \n");
+    for (let i = 0; i < matrix.length; i++){
+      // console.log(minDistance[i]);
+    }
+
+
+    // Восстановление пути
+    let ver = []; // массив посещенных вершин
+    ver[0] = end; // начальный элемент - конечная вершина
+    let k = 1; // индекс предыдущей вершины
+    let weight = minDistance[end]; // вес конечной вершины
+    // пока не дошли до начальной вершины
+    while (end != begin_index) {
+      for (let i = 0; i < matrix.length; i++) // просматриваем все вершины
+      if (matrix[i][end] != 0)   // если связь есть
+      {
+        let temp = weight - matrix[i][end]; // определяем вес пути из предыдущей вершины
+        if (temp == minDistance[i]) // если вес совпал с рассчитанным
+        {                 // значит из этой вершины и был переход
+          weight = temp; // сохраняем новый вес
+          end = i;       // сохраняем предыдущую вершину
+          ver[k] = i; // и записываем ее в массив
+          k++;
+        }
+      }
+    }
+    // Вывод пути (начальная вершина оказалась в конце массива из k элементов)
+    console.log("\nВывод кратчайшего пути\n");
+    for (let i = k - 1; i >= 0; i--)
+    console.log(`Ver${i} = `,ver[i]);
+
+    return ver;
+  }
+
 }
+
